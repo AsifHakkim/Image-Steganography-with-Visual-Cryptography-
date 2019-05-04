ba887e2d1789da8b9b86ba8ff48edaf78a18d17b
AsifHakkim <42322833+AsifHakkim@users.noreply.github.com>
2019-05-04 21:50:00 +0530

    Created WatermarkExtract.cs page
    Created image encryption method in this form

---
+++ Server side/WatermarkExtract.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//using Steganography;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Cryptic_Watermarking
+{
+    public partial class WatermarkExtract : Form
+    {
+       
+        public WatermarkExtract()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+          
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+   
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            metropanel mp = new metropanel();
+            mp.Show();
+            this.Hide();
+        }
+    }
+}
